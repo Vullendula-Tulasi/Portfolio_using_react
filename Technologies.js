92951f23a7705f4481d3c7a3416e05214d03920d
Vullendula-Tulasi <144219949+Vullendula-Tulasi@users.noreply.github.com>
2024-03-29 08:58:42 +0530

    Add files via upload

---
+++ Technologies.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./technologies.css";
+function Technologies() {
+  return (
+    <section id="technologies">
+      <span className="title">Technologies</span>
+    </section>
+  );
+}
+
+export default Technologies;
